feb86c5e492685b8227cb1531524589140ac0989
splitice <mat999@gmail.com>
2016-02-01 09:55:31 +1100

    Upgrade SystemInteract

--- YFW.Net/RuleBuilder.cs
+++ YFW.Net/RuleBuilder.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Dynamic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using SystemInteract;
 using DynamicExpresso;
 using IPTables.Net;
